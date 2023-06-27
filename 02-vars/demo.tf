7f0b377ac3fff7e0b0fae868839a1f77aeca8aae
mounika <konda2mounika@gmail.com>
2023-06-27 12:05:31 +0530

    terraform

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -10,8 +10,8 @@ output "sample-op1" {
     value = "Value is ${var.sample}"
 }
 
-A variable can be accessed with or without ${} only if the variable is called. But in case if it is needed to add with some other strings then we need to use double
-There is no concept of single quores in terraform.
+# A variable can be accessed with or without ${} only if the variable is called. But in case if it is needed to add with some other strings then we need to use double
+# There is no concept of single quores in terraform.
 
 variable "number" {
     default = 100 
