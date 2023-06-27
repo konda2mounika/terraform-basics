2ca53f783ede680ad7f1ba4a4c8cc6cca0d90e6d
mounika <konda2mounika@gmail.com>
2023-06-27 16:47:35 +0530

    terraform

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami           = "ami-0fd92c7198991d562"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "EC2-Terraform-instance"
