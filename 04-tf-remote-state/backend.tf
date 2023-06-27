d9c439823880b97751cb7defbe2eced094c6a5f8
mounika <konda2mounika@gmail.com>
2023-06-27 19:50:51 +0530

    terraform

--- 04-tf-remote-state/backend.tf
+++ 04-tf-remote-state/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket  = "b50-tf-state-bucket"
+        bucket  = "b50-tf-state-bucket-konda"
         key     = "demo/env/terrafom.tfstate"
         region  = "us-east-1"
      
